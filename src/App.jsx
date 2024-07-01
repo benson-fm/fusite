13b13585bd9fe7af120ef5e8351c20b39fb94b54
Benson Manzano <bfmanzan@uci.edu>
2024-07-01 16:21:45 -0700

    Added react-router-dom dependency and Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+
 export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  )
 }
