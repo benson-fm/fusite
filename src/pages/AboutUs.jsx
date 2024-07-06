13cfc6439c902ecfaa5f48769f247a6565013dfa
Arlaghn <apcayana@uci.edu>
2024-07-06 01:31:32 -1000

    adjusted spacing on the first section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
 
             <section className="bg-white w-full h-[750px] flex items-center justify-center relative">
                 <div className="text-center basis-[57%] transform -translate-y-1/4">
-                    <h2 className=" text-black tracking-[0.25em] text-4xl mb-8">FUSION PILLARS</h2>
+                    <h2 className=" text-black tracking-[0.25em] text-4xl mb-[55px]">FUSION PILLARS</h2>
                     <p className="text-black"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
                         dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
                         ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
@@ -60,8 +60,9 @@ const AboutUs = () => {
             <section className="bg-white w-full h-[1200px] flex flex-col items-center">
                 <h2 className="text-black text-4xl tracking-[.25em] mb-24 mt-52">ALYANSA</h2>
                 <div className="flex justify-center gap-16">
+
                     <div className="flex flex-col items-center text-center">
-                        <div className="bg-gray-300 w-64 h-64 mb-6"></div>
+                        <div className="bg-gray-300 w-64 h-64 mb-12"></div>
                         <div className="w-64 h-64">
                             <h3 className="text-black text-xl tracking-[.20em] mb-4">PASS</h3>
                             <p className="text-black mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -70,7 +71,7 @@ const AboutUs = () => {
                     </div>
                     
                     <div className="flex flex-col items-center text-center">
-                        <div className="bg-gray-300 w-64 h-64 mb-6"></div>
+                        <div className="bg-gray-300 w-64 h-64 mb-12"></div>
                         <div className="w-64 h-64">
                             <h3 className="text-black text-xl tracking-[.20em] mb-4">KABA</h3>
                             <p className="text-black mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -79,34 +80,17 @@ const AboutUs = () => {
                     </div>
 
                     <div className="flex flex-col items-center text-center">
-                        <div className="bg-gray-300 w-64 h-64 mb-6"></div>
+                        <div className="bg-gray-300 w-64 h-64 mb-12"></div>
                         <div className="w-64 h-64">
                             <h3 className="text-black text-xl tracking-[.20em] mb-4">PUSO</h3>
-                            <p className="text-black mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <p className="text-black mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         </div>                        
                         <a href="https://www.instagram.com/pusoatuci/" className="bg-yellow-500 text-black py-2 px-4 border-2 border-black tracking-[.20em] inline-block hover:scale-105 transition-transform duration-200">BUTTON</a>
                     </div>
                 </div>
             </section>
-             {/* 
-            <section className="bg-white w-full flex flex-col items-center py-16">
-                <div className="text-center max-w-2/3">
-                    <h2 className="text-black text-4xl tracking-wide mb-12">ALYANSA</h2>
-                    <div className="flex justify-center gap-16">
-                        {['PASS', 'KABA', 'PUSO'].map((item) => (
-                            <div key={item} className="flex flex-col items-center text-center">
-                                <div className="bg-gray-300 w-64 h-64 mb-6"></div>
-                                <h3 className="text-xl font-bold mb-4">{item}</h3>
-                                <p className="text-black mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                <button className="bg-[#fee270] text-black py-2 px-4 border border-black">BUTTON</button>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </section>
-            */}
             
-            <footer className="bg-[#fee270] w-full h-64">
+            <footer className="bg-[#fee270] w-full h-[355px]">
                 <p className="text-black text-2xl text-center">Footer</p>
             </footer>
         </div>
