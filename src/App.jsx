18235b78acce1fabd51d1b24b96df8c2651c3dee
Arlaghn <apcayana@uci.edu>
2024-07-06 00:03:23 -1000

    seperated the different colors for this page, as well as put the text and stuff in each section, still gotta add the alyansa part.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,71 @@
+const AboutUs = () => {
+    return (
+        <div className="flex flex-col">
+            <div className="bg-[#cbc7b7] w-full h-[840px]">
+                <div className="flex items-center justify-center h-[840px] bg-[#cbc7b7]">
+                    <p className="text-black text-6xl font-bold w-[843px] h-[155px] text-center tracking-[.5em]">ABOUT US</p>
+                </div>
+            </div>
+
+            <div className="bg-white w-full h-[750px] flex items-center justify-center relative">
+                <div className="text-center basis-[57%] transform -translate-y-1/4">
+                    <h2 className=" text-black tracking-[0.25em] text-4xl mb-8">FUSION PILLARS</h2>
+                    <p className="text-black"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                        mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+
+            <div className="bg-[#fee270] w-full h-[928px] flex items-center justify-center">
+                <div className="flex flex-wrap items-center justify-center gap-4">
+
+                    <div className="flex items-center justify-center text-black border border-black w-[200px] h-[453px] relative bg-[#cbc7b7]">
+                        <div className="text-center transform -translate-y-[15%]">
+                            <h3 className="mb-5 text-2xl tracking-[.1em]">SUPPORT</h3>
+                            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-center text-black border border-black w-[200px] h-[453px] relative bg-[#cbc7b7]">
+                        <div className="text-center transform -translate-y-[15%]"> 
+                            <h3 className="mb-5 text-2xl tracking-[.1em]">CULTURE</h3>
+                            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-center text-black border border-black w-[200px] h-[453px] relative bg-[#cbc7b7]">
+                        <div className="text-center transform -translate-y-[15%]"> 
+                            <h3 className="mb-5 text-xl">PROFESSIONALISM</h3>
+                            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-center text-black border border-black w-[200px] h-[453px] relative bg-[#cbc7b7]">
+                        <div className="text-center transform -translate-y-[15%]">
+                            <h3 className="mb-5 text-2xl">SCIENCE & ENGINEERING</h3>
+                            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-center text-black border border-black w-[200px] h-[453px] relative bg-[#cbc7b7]">
+                        <div className="text-center transform -translate-y-[15%]">
+                            <h3 className="mb-5 text-2xl">ACADEMICS</h3>
+                            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bg-white w-full h-[1200px]">
+            </div>
+            <div className="bg-[#fee270] w-full h-[355px]">
+                <p className="text-black text-2xl text-center">Footer</p>
+            </div>
+        </div>
+    );
+}
+
+export default AboutUs;
