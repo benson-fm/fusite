4e9b5f6183c2343dbc664c54b73fc20e3c65d918
EthanSantos <ethanangelo61@gmail.com>
2024-07-07 20:51:54 -0700

    issue completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
+import React from "react";
+import Footer from "../components/Footer";
+
 const Home = () => {
     return (
-        <div></div>
+        <>
+            <div></div>
+            <Footer />
+        </>
+
     );
 }
 
