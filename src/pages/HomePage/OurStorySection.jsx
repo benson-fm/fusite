da1d3b3b5f9333609213548c43e0ea6f46755e1c
Arvin Limjoco <ninjaghost041@gmail.com>
2024-07-10 22:13:19 -0700

    Final draft changes made

--- src/pages/HomePage/OurStorySection.jsx
+++ src/pages/HomePage/OurStorySection.jsx
@@ -18,16 +18,16 @@ const OurStorySection = () => {
             <span className="w-[115px] h-1 bg-yellow-500"></span>
           </div>
         </div>
-        <h2 className="text-4xl font-semibold text-black mb-6 karla-font">
+        <h2 className="text-4xl font-semibold text-black mb-6 karla-font  ">
           A direct network between <span className="text-yellow-500">Filipinx</span> and <br /><span className="text-yellow-500">engineers</span> at UC Irvine.
         </h2>
-        <p className="text-gray-700 mb-6 inter-font">
+        <p className="text-gray-700 mb-6 inter-font pb-10">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </p>
-        <div className="mt-8 flex flex-col items-center">
+        <div className="mt-8 flex flex-col items-center pt-20">
           <p className="text-lg font-bold text-black karla-font">WHY JOIN?</p>
           <button>
-          <img src={yellowDownArrow} alt="Yellow Down Arrow" className="mt-2" /> 
+          <img src={yellowDownArrow} alt="Yellow Down Arrow" className="mt-2 yellow-arrow-hover" /> 
           </button>
         </div>
       </div>
