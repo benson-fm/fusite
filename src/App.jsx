06a95914899c7f8437e9784e39cab2a06109e0ef
Arlaghn <apcayana@uci.edu>
2024-07-13 00:22:02 -1000

    Created the header and sectioned off the page into three parts. What the fam system is, the 4 fams of last year, and the fam gallery.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Family />} />
+        <Route path="/Family" element={<Family />} />
       </Routes>
     </Router>
   )
--- src/pages/Family.jsx
+++ src/pages/Family.jsx
@@ -1,13 +1,22 @@
-import React from "react";
-
 const Family = () => {
     return (
-        <>
-            <div>
-                <p>hi</p>
-            </div>
-        </>
+        <div className="flex flex-col"> {/* parent container */}
+            <header className="bg-[#cbc7b7] h-[840px] w-full flex flex-col items-center justify-center">  
+                <div className="text-center">
+                    <h1 className="text-black text-7xl font-bold tracking-[.25em]">PROGRAMS</h1>
+                    <h2 className="text-black text-4xl mt-[48px] tracking-[.25em]">FAMILY</h2>
+                </div>
+            </header>
+             {/* Entire white section 3352*/}
+            <section className="bg-white w-full h-[594px] flex items-center justify-center"> {/* What is Fam system section 594*/}
+            </section>
 
+            <section className="bg-white w-full h-[1540px] flex items-center justify-center"> {/* individual fam section 1540 */}
+            </section>
+
+            <section className="bg-white w-full h-[1218px] flex items-center justify-center"> {/* fam gallery section 1218 */}
+            </section>
+        </div>
     );
 }
 
