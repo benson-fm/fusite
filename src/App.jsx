206f48e856ae064dab47d2e565ab7feef0a13790
Arlaghn <apcayana@uci.edu>
2024-07-12 22:58:26 -1000

    made the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Family from './pages/Family'
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Family />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/Family.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Family = () => {
+    return (
+        <>
+            <div>
+                <p>hi</p>
+            </div>
+        </>
+
+    );
+}
+
+export default Family;
