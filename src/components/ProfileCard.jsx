3dfc1ab7c0b8860e58b008966a8e1527ea11adb8
Arvin Limjoco <ninjaghost041@gmail.com>
2024-07-15 14:43:00 -0700

    Added hover animation

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 const ProfileCard = ({ name, position, photoUrl, onClick }) => {
     return (
-        <div className="flex flex-col items-center cursor-pointer pb-[20px]" onClick={onClick}>
+        <div className="flex flex-col items-center pb-[20px] relative transform transition-transform duration-300 hover:scale-95 cursor-pointer" onClick={onClick}>
             <img src={photoUrl} alt={`${name}'s photo`} className="w-[389px] h-[561.89px] object-cover mb-4" />
             <h3 className="text-xl font-bold">{name}</h3>
             <p className="text-orange-500">{position}</p>
