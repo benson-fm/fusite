394bf1f21a1a5f8faed8717dee86da0a672580ef
Arlaghn <apcayana@uci.edu>
2024-07-14 18:16:26 -1000

    addd the text for fam section as well as made containers to put the content in

--- src/pages/Family.jsx
+++ src/pages/Family.jsx
@@ -1,4 +1,23 @@
 {/* w-1440 3/4 */}
+{/* 
+
+const PillarBox = ({ title, content }) => (
+    <div className="flex items-center justify-center text-black border border-black w-[225px] h-[453px] relative bg-[#cbc7b7]">
+        <div className="text-center transform -translate-y-[15%]">
+            <div className="mb-5 pb-2">
+                <h3 className="text-2xl tracking-[.1em]">{title}</h3>
+            </div>
+            <div>
+                <p className="text-center pl-4 pr-4">{content}</p>
+            </div>
+        </div>
+    </div>
+);
+*/}
+const FamPic = ({ title, image }) => (
+    <div>
+    </div>
+);
 const Family = () => {
     return (
         <div className="flex flex-col"> {/* parent container */}
@@ -24,7 +43,10 @@ const Family = () => {
                 </div>
             </section>
 
-            <section className="bg-white w-full h-[1540px] flex items-center justify-center"> {/* individual fam section 1540 */}
+            <section className="bg-white w-full h-[1540px]"> {/* individual fam section 1540 */}
+                <div className="flex items-start justify-center w-full mt-[112px]">
+                    <h1 className="text-2xl text-black tracking-[.1em]">2023-2024 FAMS</h1>
+                </div>
             </section>
 
             <section className="bg-white w-full h-[1218px] flex flex-col items-center justify-start"> {/* fam gallery section 1218 */}
