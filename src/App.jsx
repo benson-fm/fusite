d655bd89d4b52d1711a6ea68ba14e649018c5598
Dommenick Lacuata <dlacuata@uci.edu>
2024-07-16 00:43:12 -0700

    Finished Programs Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/HomePage/Home'
+import Programs from './pages/ProgramsPage/programs'
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/programs" element={<Programs/>} />
       </Routes>
     </Router>
   )
