92bdde29281647865cf6df193b1a911421f629d9
Dommenick Lacuata <dlacuata@uci.edu>
2024-07-22 02:44:20 -0700

    Completed Fundraising page and created Testimonial component to be used in other program pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import AboutUs from './pages/AboutUs'
 import Home from './pages/HomePage/Home'
+import Fundraising from './pages/ProgramsPage/FundraisingPage/Fundraising'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/programs/fundraising" element={<Fundraising/>}/>
       </Routes>
     </Router>
   )
