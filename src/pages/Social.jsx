7bf01e41e5420930fe5b9e57ec988334a486557f
Aariel <aariel.abaincia@gmail.com>
2024-08-03 18:01:55 -0700

    Social Page HIFI

--- src/pages/Social.jsx
+++ src/pages/Social.jsx
@@ -4,82 +4,107 @@ import BlueDiscord from '../assets/blueDiscord.png';
 import Valorant from '../assets/valorant.png';
 import Overwatch from '../assets/overwatch.png';
 import League from '../assets/league.png';
+import WhiteHalfSun from '../images/WhiteHalfSun.png';
+import FusionFridays from '../images/FusionFridays.png';
+import ShadowEllipse from '../images/ShadowEllipse.png';
+import GamingTopRight from '../images/GamingTopRight.png';
+import GamingBottomLeft from '../images/GamingBottomLeft.png';
+import OrangeShadowEllipse from '../images/OrangeShadowEllipse.png';
+import GamingLogos from '../images/GamingLogos.png';
+import WhiteHalfGear from '../images/WhiteHalfGear.png';
+import FtekPic from '../images/FTekPic.png';
 
 export default function Social() {
     return (
-        <div className="flex flex-col items-center justify-center w-full min-h-screen bg-white p-8">
-            <div className="max-w-[1088px] w-full flex flex-col justify-center items-center gap-10 mt-20">
-                <h1 className="text-black text-center font-inter text-4xl font-medium" style={{ letterSpacing: '8px' }}>
-                    FU-SOCIAL PROGRAMS
+        <div className="flex flex-col items-center justify-center w-full min-h-screen bg-white">
+            <div className="max-w-[1088px] w-full flex flex-col justify-center items-center gap-10 mt-40">
+                <h1 className="text-black text-center font-karla text-4xl font-bold">
+                    Make <span style={{ color: '#FFA523' }}>Connections</span> in the FUSION Space
                 </h1>
-                <p className="text-black text-center font-inter text-base leading-7 px-36">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.
-                </p>
+                <div className="w-40 border-t-4 border-solid" style={{ borderColor: '#FAC80A', opacity: 1}}></div>
+                <p className="text-black text-center font-inter text-base leading-7 px-28">
+                    FUSION's social programs are designed to enrich the member experience by fostering community, 
+                    connection, and fun. These events create opportunities for members to engage with one another 
+                    outside of academic and professional settings, building strong relationships and enhancing the 
+                    sense of camaraderie within our organization. By offering a diverse range of activities, our 
+                    social programs encourage participation, promote a balanced lifestyle, and contribute to a vibrant
+                    and supportive community.
+                </p>        
             </div>
-            <div className="max-w-[1088px] w-full flex flex-col justify-center items-center gap-10 mt-60">
-                <h1 className="text-black text-center font-inter text-4xl font-medium" style={{ letterSpacing: '8px' }}>
-                    FUSION FRIDAYS
-                </h1>
-                <p className="text-black text-center font-inter text-base leading-7 px-36">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.
-                </p>
+
+            <div className="w-full bg-[#F4F4F4] flex mt-40 overflow-x-hidden">
+                <div className="max-w-[1088px] w-full flex flex-col gap-10 relative">
+                    <img src={WhiteHalfSun} alt={`White Half Sun`} className="w-1211.6 h-964.12 left-0 object-cover flex-shrink-0 z-0"/>
+                    
+                    <img src={FusionFridays} alt={`Fusion Friday Karaoke`} className="w-1381 h-576 left-308 object-cover flex-shrink-0 absolute z-10" style={{top: '300px', left: '308px' }}/>
+                    <div className="w-[637px] h-[514px] p-[44px_0_0_0] gap-[32px] rounded-[72px_72px_72px_0] bg-white flex-shrink-0 absolute z-10" style={{top: '200px', left: '1060px' }}> 
+                        <h1 className="font-karla text-[24px] font-bold leading-[28.06px] tracking-[0.12em] text-left w-[478px] h-[28px] ml-10" style={{color: '#FFA523'}}>
+                            FUSION FRIDAYS >>
+                        </h1>
+                        <h1 className="text-black text-left font-karla text-4xl font-bold ml-10 mr-40 mt-5">
+                            FUSION Fridays: Socials That Spark Connection
+                        </h1>
+                        <div className="w-20 border-t-4 border-solid mt-10 ml-10" style={{ borderColor: '#FFA523', opacity: 1}}></div>
+                        <p className="text-black text-left font-inter text-base leading-7 ml-10 mr-10 mt-10">
+                            FUSION Fridays are all about unwinding and having fun with our vibrant community. 
+                            We host a variety of social events, from karaoke and bowling to bonfires and beyond. 
+                            These gatherings provide a relaxed atmosphere where members can bond, enjoy themselves, 
+                            and build lasting friendships outside of the academic setting. Join us for FUSION 
+                            Fridays and make the most of your FUSION experience with exciting activities and great company!
+                        </p>   
+                    </div>
+                    
+                    <img src={ShadowEllipse} alt={`Shadow Ellipse`} className="w-[420px] h-[50px] object-cover flex-shrink-0 absolute mt-10 ml-20" style={{top: '714px', left: '1060px'}}/>
+                </div>
             </div>
-            <div className="flex flex-row mt-40 gap-5">
-                <img src={FusionFriday} alt={`Fusion Friday`} className="w-307 h-307 object-cover flex-shrink-0 mt-10"/>
-                <img src={FusionFriday} alt={`Fusion Friday`} className="w-307 h-307 object-cover flex-shrink-0 mb-10"/>
-                <img src={FusionFriday} alt={`Fusion Friday`} className="w-307 h-307 object-cover flex-shrink-0 mt-10"/>
-                <img src={FusionFriday} alt={`Fusion Friday`} className="w-307 h-307 object-cover flex-shrink-0 mb-10"/>
-                <img src={FusionFriday} alt={`Fusion Friday`} className="w-307 h-307 object-cover flex-shrink-0 mt-10"/>
+
+            <div className="w-full bg-white flex overflow-hidden relative">
+                <img src={GamingTopRight} alt="Gaming Graphics Top Right" className="w-1/6 h-1/2 absolute top-0 right-0 object-cover z-0"/>
+                <img src={GamingBottomLeft} alt="Gaming Graphics Bottom Left" className="w-1/6 h-1/2 absolute bottom-0 left-0 object-cover z-0"/>
+
+                <div className="max-w-[1088px] w-full flex flex-col gap-10 relative">
+                    <div className="w-[637px] h-[514px] p-[44px_0_0_0] gap-[32px] rounded-[72px_0px_72px_72px] bg-[#F4F4F4] flex-shrink-0 z-10 ml-80 mt-20 relative"> 
+                        <h1 className="font-karla text-[24px] font-bold leading-[28.06px] tracking-[0.12em] text-left ml-10" style={{color: '#FFA523'}}>
+                            GAMING >>
+                        </h1>
+                        <h1 className="text-black text-left font-karla text-4xl font-bold ml-10 mr-20 mt-5">
+                            FUSION Frenzy: Play, Compete, and Collaborate
+                        </h1>
+                        <div className="w-20 border-t-4 border-solid mt-10 ml-10" style={{ borderColor: '#FFA523', opacity: 1}}></div>
+                        <p className="text-black text-left font-inter text-base leading-7 ml-10 mr-10 mt-10">
+                            Whether you're strategizing in a team battle or exploring new worlds, FUSION Frenzy offers a chance to unwind, bond with fellow members, and enjoy the thrill of gaming. 
+                            Join us for FUSION Frenzy and level up your FUSION experience with some friendly competition and camaraderie!
+                        </p>   
+                    </div>
+                    <img src={GamingLogos} alt="Discord, League, Overwatch, Valorant" className="w-[663.06px] h-[398.6px] object-cover absolute flex-shrink-0" style={{top: '200px', left: '1000px' }} />
+                    <img src={OrangeShadowEllipse} alt="Shadow Ellipse" className="w-[420px] h-[50px] object-cover absolute flex-shrink-0 z-10" style={{top: '620px', left: '400px' }} /> 
+                    <div className='mt-40 z-0'></div>
+                </div>
             </div>
-            <div className="max-w-[1088px] w-full flex flex-col justify-center items-center gap-10 mt-60">
-                <h1 className="text-black text-center font-inter text-4xl font-medium" style={{ letterSpacing: '8px' }}>
-                    FTEK
-                </h1>
-                <p className="text-black text-center font-inter text-base leading-7 px-36">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.
-                </p>         
+            
+            <div className="w-full bg-[#F4F4F4] flex overflow-x-hidden">
+                <div className="max-w-[1088px] w-full flex flex-col gap-10 relative">
+                    <img src={WhiteHalfGear} alt={`White Half Gear`} className="w-[806.56px] h-[820px] left-0 object-cover flex-shrink-0 z-0"/>
+                    
+                    <img src={FtekPic} alt={`F-Tek Dance Workshop`} className="w-1381 h-576 left-308 object-cover flex-shrink-0 absolute z-10" style={{top: '200px', left: '308px' }}/>
+                    <div className="w-[637px] h-[514px] p-[44px_0_0_0] gap-[32px] rounded-[72px_72px_72px_0] bg-white flex-shrink-0 absolute z-10" style={{top: '100px', left: '1060px' }}> 
+                        <h1 className="font-karla text-[24px] font-bold leading-[28.06px] tracking-[0.12em] text-left w-[478px] h-[28px] ml-10" style={{color: '#FFA523'}}>
+                            F-TEK >>
+                        </h1>
+                        <h1 className="text-black text-left font-karla text-4xl font-bold ml-10 mr-44 mt-5">
+                            Groove Into the Future: F-Tek Dance Workshops
+                        </h1>
+                        <div className="w-20 border-t-4 border-solid mt-10 ml-10" style={{ borderColor: '#FFA523', opacity: 1}}></div>
+                        <p className="text-black text-left font-inter text-base leading-7 ml-10 mr-12 mt-10">
+                            F-Tek is FUSION's dance workshop series designed to energize and inspire our members. Whether you're a seasoned dancer or just looking to have fun, 
+                            F-Tek offers engaging dance sessions that cater to all skill levels. Our workshops cover a variety of styles and techniques, providing a vibrant 
+                            space to learn new moves, express yourself, and stay active!
+                        </p>   
+                    </div>
+                    <img src={ShadowEllipse} alt={`Shadow Ellipse`} className="w-[420px] h-[50px] object-cover flex-shrink-0 absolute mt-10 ml-20" style={{top: '614px', left: '1060px'}}/>
+                </div>
             </div>
-            <div className="flex flex-row mt-40 gap-20 overflow-hidden justify-center">
-                <img src={Ftek} alt={`Ftek`} className="w-857 h-510 object-cover flex-shrink-0 mt-10"/>
-                <img src={Ftek} alt={`Ftek`} className="w-857 h-510 object-cover flex-shrink-0 mt-10"/>
-                <img src={Ftek} alt={`Ftek`} className="w-857 h-510 object-cover flex-shrink-0 mt-10"/>
-            </div>
-            <div className="max-w-[1088px] w-full flex flex-col justify-center items-center gap-10 mt-60">
-                <h1 className="text-black text-center font-inter text-4xl font-medium" style={{ letterSpacing: '8px' }}>
-                    GAMING
-                </h1>
-            </div>
-            <div className='flex flex-col items-center w-[605px] h-[290.81px]'>
-                <img src={BlueDiscord} alt='Blue Discord' className="w-[86.34px] h-[86.34px] object-cover flex-shrink-0 mt-20" />
-                <h1 className="text-black text-center font-inter text-3xl font-medium mt-10" style={{ letterSpacing: '8px' }}>
-                    DISCORD
-                </h1>
-                <p className="text-black text-center font-inter text-base leading-7 px-20 mt-10">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua.
-                </p>
-            </div>
-            <div className='flex flex-row items-center w-1274 h-252.37 gap-20'>
-                <img src={League} alt={`League`} className="w-306 h-252.31 object-cover flex-shrink-0 mt-40"/>
-                <img src={Overwatch} alt={`Overwatch`} className="w-306 h-252.37 object-cover flex-shrink-0 mt-40"/>
-                <img src={Valorant} alt={`Valorant`} className="w-306 h-247 object-cover flex-shrink-0 mt-40"/>
-            </div>
-            <h1 className="text-black text-center font-inter text-3xl font-medium mt-40" style={{ letterSpacing: '8px' }}>
-                ...AND MORE!
-            </h1>
-            <div className='mt-40'></div>
+            
         </div>
     );
 }
